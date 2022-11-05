324b46ab18f89e323e02c29220b3d2831c318f76
LenaYork <lenayork@tut.by>
2022-11-05 19:42:21 +0300

    feat:add control buttons

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import React from 'react';
 function App() {
     const [ inputText, setInputText ] = useState("");
     const [ todoArr, setTodoArr ] = useState(JSON.parse(localStorage.getItem("userList")) ?? []);
-
+    const [ activeControl, setActiveControl ] = useState("all");
+    const [ lengthTitle, setLengthTitle ] = useState("all");
+    const [ editId, setEditId ] = useState("");
 
     const inputOnChange = (event) => {
         setInputText(event.target.value);
@@ -28,6 +30,20 @@ function App() {
         setInputText("");
     }
 
+    const clearButton = () => {
+        if (todoArr.length > 0) {
+            let isConfirmed = window.confirm("Are you sure you want to delete ALL the items in the list?");
+            if(isConfirmed) {
+                setTodoArr([]);
+            }
+        } else alert("There is nothing to delete")
+    }
+
+    const activeButtonHandler = (id) => {
+        setActiveControl(id);        
+        setLengthTitle(id) 
+    }
+
     return (
     <div className="app">
         <h1 className="app-title">TODO LIST</h1>
@@ -43,6 +59,44 @@ function App() {
                     onClick={() => addItem(inputText)}    
                 />
             </div>
+
+            <div className="results-area">
+                    <div className="buttons-container">
+                        <div className="total-number-container">
+                            <div>{lengthTitle}</div>
+                            <div className="total-number">0</div>
+                        </div>
+                        <Button 
+                            innerText="all"
+                            id="all"
+                            className="control-button"
+                            activeControl={activeControl}
+                            onClick={activeButtonHandler}
+                        />
+                        <Button 
+                            innerText="done"
+                            id="done"
+                            className="control-button"
+                            activeControl={activeControl}
+                            onClick={activeButtonHandler}
+                        />
+                        <Button 
+                            innerText="pending"
+                            id="pending"
+                            className="control-button"
+                            activeControl={activeControl}
+                            onClick={activeButtonHandler}
+                        />
+                        <Button 
+                            innerText="clear"
+                            id="clear"
+                            className="clear-button"
+                            onClick={clearButton}
+                        />
+                    </div>
+
+                    {/* {renderList()} */}
+                </div>
         </div>
     </div>
     );
