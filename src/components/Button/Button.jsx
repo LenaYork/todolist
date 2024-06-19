a7bc85d23ab2d9cb090c052bbe5bd703c3d037a6
lenayork <lenayork@tut.by>
2024-06-19 22:13:29 +0200

    fix: enable done button style change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 export const Button = ({innerText, onClick, className, id, activeControl }) => {
 
   let isActiveControl = id === activeControl ? "active-control-button" : "";
-  id === "done" && (isActiveControl += "done-button");
+  id === "done" && (isActiveControl += " done-button");
 
   return (
     <div
